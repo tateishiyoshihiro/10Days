faa1c69ffa730aebbfea163cc19da0ba5303fb3a
shibuehayato <115055046+shibuehayato@users.noreply.github.com>
2024-09-09 16:33:49 +0900

    プレイヤーの移動ができた

--- Player.cpp
+++ Player.cpp
@@ -1,4 +1,6 @@
 ﻿#include "Player.h"
+#define _USE_MATH_DEFINES
+#include <math.h>
 #include<Novice.h>
 
 void Player::Initialize()
@@ -11,6 +13,28 @@ void Player::Initialize()
 	acceleration.y = 0.8f;
 	radius.x = 32;
 	radius.y = 32;
+
+	theta = 0.0f;
+	w = 63;
+	h = 63;
+	leftTopPos.x =  -w / 2;
+	leftTopPos.y =  -h / 2;
+	rightTopPos.x =  w / 2;
+	rightTopPos.y = -h / 2;
+	leftDownPos.x = -w / 2;
+	leftDownPos.y =  h / 2;
+	rightDownPos.x = w / 2;
+	rightDownPos.y = h / 2;
+
+	leftTopRotated.x = 0.0f;
+	leftTopRotated.y = 0.0f;
+	rightTopRotated.x = w / 2;
+	rightTopRotated.y = 0.0f;
+	leftDownRotated.x = 0.0f;
+	leftDownRotated.y = h / 2;
+	rightDownRotated.x = w / 2;
+	rightDownRotated.y = h / 2;
+	grHandle = Novice::LoadTexture("white1x1.png");
 }
 
 void Player::Update()
@@ -26,15 +50,46 @@ void Player::Update()
 
 	if (keys[DIK_A])
 	{
+		theta -= 0.1f;
 		pos.x -= speed.x;
 	}
 	if (keys[DIK_D])
 	{
+		theta += 0.1f;
 		pos.x += speed.x;
 	}
+
+	leftTopRotated.x = (leftTopPos.x * cosf(theta) - leftTopPos.y * sinf(theta)) + pos.x;
+	leftTopRotated.y = (leftTopPos.y * cosf(theta) + leftTopPos.x * sinf(theta)) + pos.y;
+	rightTopRotated.x = (rightTopPos.x * cosf(theta) - rightTopPos.y * sinf(theta)) + pos.x;
+	rightTopRotated.y = (rightTopPos.y * cosf(theta) + rightTopPos.x * sinf(theta)) + pos.y;
+	leftDownRotated.x = (leftDownPos.x * cosf(theta) - leftDownPos.y * sinf(theta)) + pos.x;
+	leftDownRotated.y = (leftDownPos.y * cosf(theta) + leftDownPos.x * sinf(theta)) + pos.y;
+	rightDownRotated.x = (rightDownPos.x * cosf(theta) - rightDownPos.y * sinf(theta)) + pos.x;
+	rightDownRotated.y = (rightDownPos.y * cosf(theta) + rightDownPos.x * sinf(theta)) + pos.y;
+
+	/*leftTopRotated.x += 63;
+	leftTopRotated.y += 63;
+	rightTopRotated.x += 63;
+	rightTopRotated.y += 63;
+	leftDownRotated.x += 63;
+	leftDownRotated.y += 63;
+	rightDownRotated.x += 63;
+	rightDownRotated.y += 63;*/
+
 }
 
 void Player::Draw()
 {
+	// 当たり判定するようの円
 	Novice::DrawEllipse((int)pos.x, (int)pos.y, (int)radius.x, (int)radius.y, 0.0f, WHITE, kFillModeSolid);
+
+	Novice::DrawQuad((int)leftTopRotated.x, (int)leftTopRotated.y,
+		(int)rightTopRotated.x, (int)rightTopRotated.y,
+		(int)leftDownRotated.x, (int)leftDownRotated.y,
+		(int)rightDownRotated.x, (int)rightDownRotated.y,
+		0, 0,
+		(int)w, (int)h,
+		grHandle,
+		WHITE);
 }
--- Player.h
+++ Player.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 #include "Vector2.h"
 
 struct Player
@@ -7,8 +7,21 @@ public:
 	Vector2 pos;
 	Vector2 radius;
 	Vector2 speed;
-	Vector2 acceleration;
+	Vector2 acceleration; // 重力
     char keys[256];
+
+	float w;
+	float h;
+	Vector2 leftTopPos;
+	Vector2 rightTopPos;
+	Vector2 leftDownPos;
+	Vector2 rightDownPos;
+	float theta;
+	Vector2 leftTopRotated;
+	Vector2 rightTopRotated;
+	Vector2 leftDownRotated;
+	Vector2 rightDownRotated;
+	int grHandle;
 
 public:
 	void Initialize();
